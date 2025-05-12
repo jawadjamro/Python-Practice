ef33246e9dc3be3a9f915b0e4b2c10d63a4ce919
Jawad Hassan <159148373+jawadjamro@users.noreply.github.com>
2025-05-12 11:26:09 +0500

    Add files via upload

---
+++ variables and data-types/main.py
@@ -0,0 +1,46 @@
+print("Jawad Hassan")
+print(30 + 40)
+
+# Variables
+name = "jawad hassan"
+age = 20 
+city = "karachi"
+
+age2 = age
+print(age2)
+
+name1 = "jawad"
+name2 = '''hassan'''
+name3 = 'jamro'
+
+print(name1)
+print(name2)
+print(name3)
+
+age = 20
+old = False
+a = None
+
+print(type(age))
+print(type(old))
+print(type(a))
+
+a = 20
+b = 30
+sum = a - b
+
+print(sum)
+
+
+
+name = input("name : ")
+age = int(input("age :"))
+price = float(input("price"))
+
+print("My name is ",name,"and iam",age,"years old")
+
+
+city = input("city :")
+village = int(input("village :"))
+country = float(input("country"))
+
