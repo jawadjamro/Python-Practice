d91630826350b8cd2a6255b6c40a4f09f4477c6c
Jawad Hassan <159148373+jawadjamro@users.noreply.github.com>
2025-05-14 09:53:47 +0500

    Add files via upload

---
+++ operators/main.py
@@ -0,0 +1,59 @@
+# Arithmetic operators
+a = 10
+b = 5
+
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a % b)  # Reminder
+
+# Relational operators
+a = 50
+b = 20
+
+print(a == b)  # False
+print(a != b)  # True
+print(a >= b)  # True
+print(a > b)   # True
+print(a <= b)  # False
+print(a < b)   # False
+
+# Assignment operators
+num = 10
+num += 5
+print("num :", num)  # 15
+
+num = 5
+num -= 5
+print("num :", num)  # 0
+
+num = 20
+num *= 5
+print("num :", num)  # 100
+
+num = 10
+num /= 4
+print("num :", num)  # 2.5
+
+num = 10
+num **= 10
+print("num :", num)  # 10000000000
+
+# Logical operators
+a = 50
+b = 20
+
+print(not False)
+print(not (a > b))
+
+val1 = True
+val2 = False
+
+print("AND operator:", val1 and val2)
+
+# Type conversion
+a = 2
+b = 4.25
+sum = a + b  # 6.25
+print("sum")
