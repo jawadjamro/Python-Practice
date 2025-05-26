c5101bb8c5ebeb7d619dc29f947e2ef630e30299
Jawad Hassan <159148373+jawadjamro@users.noreply.github.com>
2025-05-26 11:12:08 +0500

    Add files via upload

---
+++ strings/main.py
@@ -0,0 +1,73 @@
+# str1 = "My name is Jawad"
+# str2 = "Iam a programmer"
+# str3 = "i work at a software house"
+
+# "Pakistani's are the best"
+
+
+# str = "Iam a Giaic student.\nI lives in Gambat"
+# print(str)
+
+#concatination
+# str1 = "Jawad"
+# len1 = len(str1)
+# print(len1)
+
+
+# str2 = "Hassan"
+# len2 = len(str2)
+# print(len2)
+
+# final_str = str1+str2
+# print(final_str)
+
+
+# #indexing
+# str1 = "Jawad Hassan"
+# ch = str1[0]
+# print(ch)
+
+
+# #slicing
+# str = "Jawad Hassan" 
+# print(str[:5]) #slicing from 0 to 5
+# print(str[6:]) #slicing from 6 to end
+
+
+# str = "Jawad Hassan"
+# print(str.endswith("Hassan")) #check if string ends with Hassan
+
+
+# str = "jawad Hassan"
+# print(str.capitalize())
+
+# str = "Jawad Hassan"
+# print(str.replace("Jawad","Fawad")) #replace Jawad with Fawad
+
+
+# str ="Jawad Hassan"
+# print(str.find("ss")) #find the index of s
+
+
+# str = "Jawad Hassan"
+# print(str.count("a")) #count the number of a in the string
+
+# age = 21 
+
+# if (age >= 18):
+#     print("You are eligible to vote")
+
+
+# Conditional statements
+marks = int(input("Enter student marks: "))  # Convert input to an integer
+
+if marks >= 90:
+    grade = "A"
+elif 80 <= marks < 90:
+    grade = "B"
+elif 70 <= marks < 80:
+    grade = "C"
+else:
+    grade = "D"
+
+print("Grade of the student ->", grade)
