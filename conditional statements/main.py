8e7a81330767cbd0578e5f503b580a59af83abca
Jawad Hassan <159148373+jawadjamro@users.noreply.github.com>
2025-05-26 11:13:01 +0500

    Add files via upload

---
+++ conditional statements/main.py
@@ -0,0 +1,26 @@
+light = input("light color :")
+if (light == "red"):
+    print("stop")
+
+elif(light == "yellow"):
+    print("look")
+
+elif(light == "green"):
+    print("go")
+
+else:
+    print("light is broken")
+
+
+light = input("light color :")
+if (light == "red"):
+    print("see")
+
+elif(light == "white"):
+    print("see")
+
+elif(light == "pink"):
+    print("go")
+else:
+    print("No you go")
+
